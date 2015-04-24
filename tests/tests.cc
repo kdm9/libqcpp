671124ce038ceb7287c5d7e8f3247ebe638ac4c8
Kevin Murray <spam@kdmurray.id.au>
2015-04-24 10:34:01 +1000

    Add initial test file setup

---
+++ tests/tests.cc
@@ -0,0 +1,14 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  tests.cc
+ *    Description:  libqc++ test main source. Includes all tests
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
