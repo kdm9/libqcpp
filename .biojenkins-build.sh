8fc42e6d865da340ffb3ec4cc4504d84d7fd50ff
Kevin Murray <spam@kdmurray.id.au>
2015-04-25 23:20:37 +1000

    Test parsing w/ qcparse in biojenkins script

--- .biojenkins-build.sh
+++ .biojenkins-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 wdir=`pwd`
 
 rm -rf build
@@ -7,3 +9,14 @@ mkdir build && cd build
 
 cmake $wdir
 make
+
+# for use in tests below
+tmpdir=biojenkins-out/$RANDOM
+mkdir -p $tmpdir
+
+# test parsing of valid file using qcparse
+./bin/qcparse ./data/valid.fastq >$tmpdir/valid.fastq
+cmp --silent ./data/valid.fastq $tmpdir/valid.fastq
+
+# clean up from above tests
+rm -rf $tmpdir
