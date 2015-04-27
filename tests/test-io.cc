77ca0131475a318a20e6aa2e9eccaa80fe39f1c1
Kevin Murray <spam@kdmurray.id.au>
2015-04-27 22:37:32 +1000

    More IO module tests -- mostly parsing code

--- tests/test-io.cc
+++ tests/test-io.cc
@@ -50,7 +50,7 @@ TEST_CASE("Read structure behaves correctly", "[Read]") {
 TEST_CASE("ReadParser opening works", "[ReadParser]") {
     qcpp::ReadParser parser;
     TestConfig *config = TestConfig::get_config();
-    std::string infile = config->get_data_file("valid.fastq");
+    std::string infile = config->get_data_file("valid_il.fastq");
 
     SECTION("opening valid FASTQ as std::string works") {
         REQUIRE_NOTHROW(parser.open(infile));
@@ -61,11 +61,112 @@ TEST_CASE("ReadParser opening works", "[ReadParser]") {
     }
 }
 
-TEST_CASE("Valid Fastq Reading", "[ReadParser]") {
+TEST_CASE("Valid file parsing, including get_num_reads", "[ReadParser]") {
     qcpp::Read read;
     qcpp::ReadParser parser;
     TestConfig *config = TestConfig::get_config();
-    std::string infile = config->get_data_file("valid.fastq");
+    std::string infile;
+    size_t n_reads = 0;
+
+    SECTION("valid_il.fastq") {
+        infile = config->get_data_file("valid_il.fastq");
+
+        REQUIRE_NOTHROW(parser.open(infile));
+
+        // Count all reads, parse_read returns false on EOF
+        while (parser.parse_read(read)) {
+            n_reads++;
+        }
+
+        REQUIRE(n_reads == 10);
+        REQUIRE(parser.get_num_reads() == n_reads);
+    }
+
+    SECTION("valid.fasta") {
+        infile = config->get_data_file("valid.fasta");
+
+        REQUIRE_NOTHROW(parser.open(infile));
+
+        // Count all reads, parse_read returns false on EOF
+        while (parser.parse_read(read)) {
+            n_reads++;
+        }
+
+        REQUIRE(n_reads == 10);
+        REQUIRE(parser.get_num_reads() == n_reads);
+    }
+}
+
+TEST_CASE("Parsing of invalid or weird files", "[ReadParser]") {
+    qcpp::Read              read;
+    qcpp::ReadParser        parser;
+    TestConfig             *config = TestConfig::get_config();
+    std::string             infile;
+    size_t                  n_reads = 0;
+
+
+    SECTION("Empty fastq") {
+        infile = config->get_data_file("empty.fastq");
+        REQUIRE_THROWS_AS(parser.open(infile), qcpp::IOError);
+
+        while (parser.parse_read(read)) {
+            n_reads++;
+        }
+
+        REQUIRE(n_reads == 0);
+        REQUIRE(parser.get_num_reads() == n_reads);
+    }
+
+    SECTION("Truncated fastq") {
+        infile = config->get_data_file("truncated.fastq");
+        REQUIRE_NOTHROW(parser.open(infile));
+
+        REQUIRE_NOTHROW(parser.parse_read(read)); // First read is OK
+        REQUIRE_THROWS_AS(parser.parse_read(read), qcpp::IOError); // 2nd bad
+    }
+}
+
+TEST_CASE("Read Interleaving", "[ReadInterleaver]") {
+    qcpp::ReadPair          read_parser_pair;
+    qcpp::ReadPair          read_interleaver_pair;
+    qcpp::ReadParser        parser;
+    qcpp::ReadInterleaver   interleaver;
+    TestConfig             *config = TestConfig::get_config();
+    std::string             il_file = config->get_data_file("valid_il.fastq");
+    std::string             r1_file = config->get_data_file("valid_R1.fastq");
+    std::string             r2_file = config->get_data_file("valid_R2.fastq");
+
+    REQUIRE_NOTHROW(parser.open(il_file));
+    REQUIRE_NOTHROW(interleaver.open(r1_file, r2_file));
+
+    SECTION("Interleaving yields a correctly paired read stream") {
+        size_t n_pairs = 0;
+        bool parse_ret = true, il_ret = true;
+
+        // Count all reads, parse_read returns false on EOF
+        while (parse_ret && il_ret) {
+            parse_ret = parser.parse_read_pair(read_parser_pair);
+            il_ret = interleaver.parse_read_pair(read_interleaver_pair);
+            REQUIRE(read_parser_pair == read_interleaver_pair);
+            if (parse_ret && il_ret) {
+                n_pairs++;
+            }
+        }
+        REQUIRE(parse_ret == il_ret);
+
+        size_t n_reads = n_pairs * 2;
+        REQUIRE(n_reads == 10);
+        REQUIRE(parser.get_num_reads() == n_reads);
+        REQUIRE(interleaver.get_num_reads() == n_reads);
+        REQUIRE(interleaver.get_num_pairs() == n_pairs);
+    }
+}
+
+TEST_CASE("Fastq writing", "[ReadParser]") {
+    qcpp::Read read;
+    qcpp::ReadParser parser;
+    TestConfig *config = TestConfig::get_config();
+    std::string infile = config->get_data_file("valid_il.fastq");
     REQUIRE_NOTHROW(parser.open(infile));
 
     SECTION("Parsing a valid fastq works, including get_num_reads") {
@@ -80,3 +181,4 @@ TEST_CASE("Valid Fastq Reading", "[ReadParser]") {
         REQUIRE(parser.get_num_reads() == n_reads);
     }
 }
+
