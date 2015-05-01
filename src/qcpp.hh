41d8ce07d7441688c37e115678ffcb2541c3286e
Kevin Murray <spam@kdmurray.id.au>
2015-05-01 21:09:59 +1000

    Add global header

---
+++ src/qcpp.hh
@@ -0,0 +1,20 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qcpp.hh
+ *    Description:  Global header to include all subheaders
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#ifndef QCPP_HH
+#define QCPP_HH
+
+#include "qc-config.hh"
+#include "qc-io.hh"
+#include "qc-processor.hh"
+#include "qc-length.hh"
+
+#endif /* QCPP_HH */
