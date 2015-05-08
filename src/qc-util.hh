1e74ef1130cc50edff92d2162c6cf020db089e39
Kevin Murray <spam@kdmurray.id.au>
2015-05-08 15:38:16 +1000

    Add util header, with atomics removed

---
+++ src/qc-util.hh
@@ -0,0 +1,22 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qc-util.hh
+ *    Description:  Utilities for qcpp
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#ifndef QC_UTIL_HH
+#define QC_UTIL_HH
+
+#include "qc-config.hh"
+
+namespace qcpp
+{
+
+}
+
+#endif /* QC_UTIL_HH */
