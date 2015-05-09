581cb457ff22c77cd8c9ce7b91a6be1988c1e6b7
Kevin Murray <spam@kdmurray.id.au>
2015-05-10 01:20:17 +1000

    Rewrite qcpipeline.cc demo script

--- util/qcpipeline.cc
+++ util/qcpipeline.cc
@@ -24,6 +24,7 @@ main (int argc, char *argv[])
 {
     std::ofstream yml_output;
     qcpp::ProcessedReadStream stream;
+    uint64_t n_pairs = 0;
 
     if (argc != 3) {
         std::cerr << "USAGE: " << argv[0] << " <read_file> <yml_file>"
@@ -33,18 +34,26 @@ main (int argc, char *argv[])
 
     stream.open(argv[1]);
     yml_output.open(argv[2]);
-    stream.append_processor<qcpp::PerBasePhredHistogram>("before qc");
+    stream.append_processor<qcpp::PerBaseQuality>("before qc");
     stream.append_processor<qcpp::ReadLenFilter>("trim at 50", 50);
     stream.append_processor<qcpp::ReadLenCounter>("after qc");
 
-    // to enable paralleism do this
+    // to enable paralleism uncomment these
     //#pragma omp parallel
     {
         qcpp::ReadPair rp;
         while (stream.parse_read_pair(rp)) {
-            // std::cout << rp.str();
+            // __sync_fetch_and_add(&n_pairs, 1);
+            if (n_pairs++ % 100000 == 0) {
+                std::cerr << "Processed " << n_pairs << "\r";
+            }
+            //#pragma omp critical
+            {
+                // std::cout << rp.str();
+            }
         }
     }
+    std::cerr << "Processed " << n_pairs << std::endl;
     yml_output << stream.report();
     return EXIT_SUCCESS;
 }
