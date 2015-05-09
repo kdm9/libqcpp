3704c0a56d98f5c188e9ca2008413e0da10419ad
Kevin Murray <spam@kdmurray.id.au>
2015-05-10 01:15:31 +1000

    Add QC measurement code

---
+++ src/qc-measure.cc
@@ -0,0 +1,150 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qc-measure.cc
+ *    Description:  Collect measures of read quality
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#include <yaml-cpp/yaml.h>
+
+#include "qc-measure.hh"
+
+namespace qcpp
+{
+
+/////////////////////////////////// BASE CLASS ////////////////////////////////
+QCMeasure::
+QCMeasure(const std::string &name, unsigned phred_offset):
+    ReadProcessor(name),
+    _have_r2(false),
+    _phred_offset(phred_offset)
+{
+}
+
+
+/////////////////////////////// PerBaseQuality /////////////////////////
+PerBaseQuality::
+PerBaseQuality(const std::string &name, unsigned phred_offset):
+    QCMeasure(name, phred_offset),
+    _max_len(0)
+{
+}
+
+void
+PerBaseQuality::
+process_read(Read &the_read)
+{
+    size_t read_len = the_read.size();
+    if (read_len > _max_len) {
+        std::lock_guard<std::mutex> lg(_expansion_mutex);
+        for (size_t i = _max_len; i <= read_len; i++) {
+            _qual_scores_r1.emplace_back();
+        }
+        _max_len = read_len;
+    }
+
+    for (size_t i = 0; i < read_len; i++) {
+        size_t qual_score = the_read.quality[i] - _phred_offset;
+        // It's a kludge, but we have to use uint64_t and to sync_add here, as
+        // std::atomic doesn't like being in a std::array
+        __sync_add_and_fetch(&_qual_scores_r1[i][qual_score], 1);
+    }
+    _num_reads++;
+}
+
+void
+PerBaseQuality::
+process_read_pair(ReadPair &the_read_pair)
+{
+    size_t read_len1 = the_read_pair.first.size();
+    size_t read_len2 = the_read_pair.second.size();
+    const Read &r1 = the_read_pair.first;
+    const Read &r2 = the_read_pair.second;
+    size_t larger_len = read_len1 > read_len2 ? read_len1 : read_len2;
+
+    _have_r2 = true;
+    if (larger_len > _max_len) {
+        std::lock_guard<std::mutex> lg(_expansion_mutex);
+        for (size_t i = _max_len + 1; i <= larger_len; i++) {
+            _qual_scores_r1.emplace_back();
+            _qual_scores_r2.emplace_back();
+        }
+        _max_len = larger_len;
+    }
+    for (size_t i = 0; i < read_len1; i++) {
+        size_t qual_score = r1.quality[i] - _phred_offset;
+        // It's a kludge, but we have to use uint64_t and to sync_add here, as
+        // std::atomic doesn't like being in a std::array
+        __sync_add_and_fetch(&_qual_scores_r1[i][qual_score], 1);
+    }
+    for (size_t i = 0; i < read_len2; i++) {
+        size_t qual_score = r2.quality[i] - _phred_offset;
+        __sync_add_and_fetch(&_qual_scores_r2[i][qual_score], 1);
+    }
+    _num_reads += 2;
+}
+
+std::string
+PerBaseQuality::
+report()
+{
+    using namespace YAML;
+    std::ostringstream ss;
+    YAML::Emitter yml;
+
+    yml << BeginSeq;
+    yml << BeginMap;
+    yml << Key << "PerBaseQuality"
+        << Value
+        << BeginMap
+        << Key   << "name"
+        << Value << _name
+        << Key   << "parameters"
+        << Value << BeginMap
+                 << Key << "phred_offset"
+                 << Value << _phred_offset
+                 << EndMap
+        << Key   << "output"
+        << Value << BeginMap
+                 << Key << "num_reads"
+                 << Value << _num_reads
+                 << Key << "r1_phred_scores"
+                 << Value << BeginSeq;
+    // Handle R1 phred scores
+    for (size_t i = 0; i < _max_len; i++) {
+        yml << Flow;
+        yml << BeginSeq;
+        for (size_t j = 0; j < _qual_scores_r1[i].size(); j++) {
+            yml << _qual_scores_r1[i][j];
+        }
+        yml << EndSeq;
+    }
+    yml                   << EndSeq; // End of r1_phred_scores
+    yml          << Key << "r2_phred_scores"
+                 << Value << BeginSeq;
+    if (_have_r2) {
+        // Handle R2 phred scores
+        for (size_t i = 0; i < _max_len; i++) {
+            yml << Flow;
+            yml << BeginSeq;
+            for (size_t j = 0; j < _qual_scores_r2[i].size(); j++) {
+                yml << _qual_scores_r2[i][j];
+            }
+            yml << EndSeq;
+        }
+    }
+    yml                   << EndSeq; // End of r2_phred_scores
+
+    yml          << EndMap
+        << EndMap;
+    yml << EndMap;
+    yml << EndSeq;
+    ss << yml.c_str() << "\n";
+    return ss.str();
+}
+
+} // end namespace qcpp
