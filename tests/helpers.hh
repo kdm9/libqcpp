e359711f0bf9fa8753d64dc02720811b7641ccee
Kevin Murray <spam@kdmurray.id.au>
2015-05-11 19:31:43 +1000

    Add initial tests of the length module, move catch
    - Move catch to tests/ext/
    - Implement file comparision functions in helpers
    - Implement tests of ReadLenCounter, add data files

--- tests/helpers.hh
+++ tests/helpers.hh
@@ -16,7 +16,10 @@
 #include <cstdlib>
 #include <cstdio>
 #include <sstream>
+#include <iostream>
 #include <string>
+#include <fstream>
+
 
 class TestConfig
 {
@@ -99,4 +102,73 @@ protected:
 };
 
 extern TestConfig _config;
+
+// returns true on identity, false otherwise
+static inline bool
+filecmp(const std::string &filename1,
+        const std::string &filename2)
+{
+    std::ifstream fp1(filename1), fp2(filename2);
+
+    if (!fp1 || !fp2) {
+        return false;
+    }
+
+    if (std::equal(std::istreambuf_iterator<char>(fp1),
+                   std::istreambuf_iterator<char>(),
+                   std::istreambuf_iterator<char>(fp2))) {
+        return true;
+    }
+    return false;
+}
+// returns true on identity, false otherwise
+static inline bool
+filecmp2(const std::string &filename1,
+         const std::string &filename2)
+{
+    char chr1, chr2;
+    std::ifstream fp1(filename1), fp2(filename2);
+
+    if (!fp1 || !fp2) {
+        return false;
+    }
+
+    while (fp1.good() && fp2.good()) {
+        fp1.get(&chr1, 1);
+        fp2.get(&chr2, 1);
+        if (chr1 != chr2) {
+            return false;
+        }
+    }
+    if (fp1.eof() && fp2.eof()) {
+        return true;
+    }
+    return false;
+}
+
+// returns true on identity, false otherwise
+static inline bool
+filestrcmp(const std::string &filename,
+           const std::string &contents)
+{
+    std::ifstream fp(filename, std::ios_base::binary);
+    size_t idx = 0;
+
+    if (!fp) {
+        return false;
+    }
+
+    while (fp.good() && idx < contents.size()) {
+        char chr = fp.get();
+        if (chr != contents[idx]) {
+            return false;
+        }
+        idx++;
+    }
+    if (idx == contents.size() && fp.get() == EOF) {
+        return true;
+    }
+    return false;
+}
+
 #endif /* HELPERS_HH */
---
+++ tests/test-length.cc
@@ -0,0 +1,54 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  test-length.cc
+ *    Description:  Tests of the length module.
+ *        License:  LGPL-3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+
+#include "catch.hpp"
+#include "helpers.hh"
+
+#include "qc-io.hh"
+#include "qc-length.hh"
+
+TEST_CASE("QC length counter works", "[ReadLenCounter]") {
+    qcpp::ReadParser        parser;
+    qcpp::ReadLenCounter    rlc("A Read Length Counter");
+    TestConfig             *config = TestConfig::get_config();
+    std::string             infile = config->get_data_file("valid_il.fastq");
+    std::string             se_report_file = config->get_data_file(
+                                    "qc_reports/report_ReadLenCounter_se.yml");
+    std::string             pe_report_file = config->get_data_file(
+                                    "qc_reports/report_ReadLenCounter_pe.yml");
+    size_t                  n_reads = 0;
+    std::string             report;
+
+    parser.open(infile);
+    SECTION("Single-end works") {
+        qcpp::Read read;
+        while (parser.parse_read(read)) {
+            rlc.process_read(read);
+            n_reads++;
+        }
+
+        report = rlc.report();
+        REQUIRE(n_reads == 10);
+        REQUIRE(filestrcmp(se_report_file, report));
+    }
+    SECTION("Paired-end works") {
+        qcpp::ReadPair pair;
+        while (parser.parse_read_pair(pair)) {
+            rlc.process_read_pair(pair);
+            n_reads += 2;
+        }
+
+        report = rlc.report();
+        REQUIRE(n_reads == 10);
+        REQUIRE(filestrcmp(pe_report_file, report));
+    }
+}
