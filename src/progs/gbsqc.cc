7044a25384e8da938f3d9f2c216313da90dfba94
Kevin Murray <spam@kdmurray.id.au>
2015-08-10 18:37:13 +1000

    New gbsqc program, pre trim

--- src/progs/gbsqc.cc
+++ src/progs/gbsqc.cc
@@ -13,18 +13,33 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <iomanip>
 
 #include "qcpp.hh"
 
 #include "qc-measure.hh"
 #include "qc-length.hh"
+#include "qc-qualtrim.hh"
+#include "qc-gbs.hh"
+
+using std::chrono::system_clock;
+
+inline void
+progress(size_t n, system_clock::time_point start)
+{
+    std::chrono::duration<double> tdiff = system_clock::now() - start;
+    double secs = tdiff.count();
+    double k_reads = n / 1000.0;
+    double rate = k_reads / secs;
+    std::cerr << "\x1b[2K" << "Kept " << k_reads << "K read pairs in "
+              << (int)secs << "s (" << std::setprecision(3) << rate
+              << std::setprecision(9) << "K RP/sec)\r";
+}
 
 int
 main (int argc, char *argv[])
 {
-    std::ofstream yml_output;
-    qcpp::ProcessedReadStream stream;
-    uint64_t n_pairs = 0;
 
     if (argc != 3) {
         std::cerr << "USAGE: " << argv[0] << " <read_file> <yml_file>"
@@ -32,28 +47,38 @@ main (int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    stream.open(argv[1]);
-    yml_output.open(argv[2]);
-    stream.append_processor<qcpp::PerBaseQuality>("before qc");
-    stream.append_processor<qcpp::ReadLenFilter>("trim at 50", 50);
-    stream.append_processor<qcpp::ReadLenCounter>("after qc");
+    qcpp::ProcessedReadStream   stream(argv[1]);
+    std::ofstream               yml_output(argv[2]);
+    uint64_t                    n_pairs = 0;
 
+    //stream.append_processor<qcpp::PerBaseQuality>("before qc");
+    stream.append_processor<qcpp::GBSTrimPE>("trim Pst1 read-through", "CTGCAG", 1);
+    stream.append_processor<qcpp::WindowedQualTrim>("QC", 28, 33, 50);
+    //stream.append_processor<qcpp::PerBaseQuality>("after qc");
+
+    system_clock::time_point start = system_clock::now();
     // to enable paralleism uncomment these
     //#pragma omp parallel
     {
+        //std::ostringstream oss;
         qcpp::ReadPair rp;
         while (stream.parse_read_pair(rp)) {
-            // __sync_fetch_and_add(&n_pairs, 1);
-            if (n_pairs++ % 100000 == 0) {
-                std::cerr << "Processed " << n_pairs << "\r";
+            if (n_pairs % 100000 == 0) {
+                //#pragma omp critical
+                {
+                    progress(n_pairs, start);
+                    //std::cout << oss.str();
+                }
+                //oss.str("");
+                //oss.clear();
             }
-            //#pragma omp critical
-            {
-                // std::cout << rp.str();
-            }
+            //oss << rp.str();
+            //__sync_fetch_and_add(&n_pairs, 1);
+            n_pairs++;
         }
     }
-    std::cerr << "Processed " << n_pairs << std::endl;
+    progress(n_pairs, start);
+    std::cerr << std::endl;
     yml_output << stream.report();
     return EXIT_SUCCESS;
 }
