293723c4f0b07c2ee19acb1ec730c0adc6f6bc5e
Kevin Murray <spam@kdmurray.id.au>
2016-04-07 11:35:18 +1000

    Add static build config w/ phusion's HBB

---
+++ src/hbb-build.sh
@@ -0,0 +1,45 @@
+#########
+# SETUP #
+#########
+set -e
+source /hbb_exe_gc_hardened/activate
+set -x
+
+export QCPP_VER=$1
+
+cd $(mktemp -d)
+
+########
+# DEPS #
+########
+
+# Install boost headers only
+tar xf /io/boost.tar*
+mv boost_1_*/boost /usr/local/include
+rm -rf boost*/
+
+# Compile & install yaml-cpp 
+tar xf /io/yaml-cpp.tar*
+pushd yaml-cpp*/
+mkdir build && cd build
+cmake .. -DYAML_CPP_BUILD_TOOLS=Off -DYAML_CPP_BUILD_CONTRIB=Off
+make -j4
+make install
+popd
+rm -rf yaml-cpp*/
+
+
+#########
+# BUILD #
+#########
+
+prefix=/io/libqcpp_${QCPP_VER}_amd64
+mkdir -p $prefix
+
+builddir=$(mktemp -d)
+pushd $builddir
+
+cmake /io -DCMAKE_INSTALL_PREFIX=$prefix -DQCPP_ENABLE_TESTS=Off
+make -j4
+make install
+popd
