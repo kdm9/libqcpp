3f4bc0a9aa2cef5d6d284426690eb97f64a885a3
Kevin Murray <spam@kdmurray.id.au>
2016-11-30 16:40:29 +1100

    Fix static build to use my hbb fork

--- src/hbb-build.sh
+++ src/hbb-build.sh
@@ -2,55 +2,60 @@
 # SETUP #
 #########
 set -e
-source /hbb_exe_gc_hardened/activate
+source /hbb_exe/activate
 set -x
 
-export QCPP_VER=$1
+QCPP_VERSION=$1
+
+YAMLCPP_VER=0.5.3
 
 cd $(mktemp -d)
+export  CMAKE_PREFIX_PATH=/hbb_exe
 
 ########
 # DEPS #
 ########
 
-# Install libbz2
-tar xf /io/deps/libbz2.tar*
-pushd bzip2*/
-make CFLAGS=-fPIC
-make install
-popd
-rm -rf boost*/
-
-# Install boost headers only
-tar xf /io/deps/boost.tar* boost_1_60_0/boost/
-mv boost_1_*/boost /usr/local/include
-rm -rf boost*/
-
 # Compile & install yaml-cpp 
-tar xf /io/deps/yaml-cpp.tar*
+curl -LSO https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-${YAMLCPP_VER}.tar.gz
+tar xf yaml-cpp*.tar.gz
 pushd yaml-cpp*/
 mkdir build && cd build
-cmake .. -DYAML_CPP_BUILD_TOOLS=Off -DYAML_CPP_BUILD_CONTRIB=Off
+cmake ..                       \
+    -DYAML_CPP_BUILD_TOOLS=Off \
+    -DYAML_CPP_BUILD_CONTRIB=Off
 make -j4
 make install
 popd
-rm -rf yaml-cpp*/
+rm -rf yaml-cpp*
 
+yum install -y git
 
 #########
 # BUILD #
 #########
 
-prefix=/io/libqcpp_${QCPP_VER}_amd64
+
+prefix=/io/static_build
+rm -rf $prefix
 mkdir -p $prefix
+chown 1000:1000 -R $prefix
 
+# Clone to builddir
 builddir=$(mktemp -d)
+git clone /io $builddir
 pushd $builddir
+git reset --hard HEAD
 
-cmake /io -DCMAKE_INSTALL_PREFIX=$prefix
-make -j4
+mkdir build
+cd build
+
+cmake $builddir                    \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DSTATIC_BINARIES=On           \
+    -DQCPP_VERSION=$QCPP_VERSION
+make -j4 VERBOSE=1
 make test
 make install
-popd
 
 chown 1000:1000 -R $prefix
